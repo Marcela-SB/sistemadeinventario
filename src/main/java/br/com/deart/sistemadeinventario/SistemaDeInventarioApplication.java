a33c4f8a35812356655d9703333dc4363f4908f3
Marcela <marcela013sb@hotmail.com>
2025-08-01 21:04:49 -0300

    first commit

---
+++ src/main/java/br/com/deart/sistemadeinventario/SistemaDeInventarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.deart.sistemadeinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/deart/sistemadeinventario/SistemaDeInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deart.sistemadeinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
